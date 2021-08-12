2d00c259af5c53f870b31c69d9d86ea5afc437c8
Mokshit06 <50412128+Mokshit06@users.noreply.github.com>
2021-08-12 22:23:49 +0530

    setup subdomains in express

--- packages/server/src/index.ts
+++ packages/server/src/index.ts
@@ -1,9 +1,30 @@
 import express from 'express';
+import store from './apps/store';
+import platform from './apps/platform';
+
+declare global {
+  namespace Express {
+    export interface Request {
+      store?: string;
+    }
+  }
+}
 
 const app = express();
 
-app.get('/', (req, res) => {
-  res.send('works');
+app.use((req, res) => {
+  const { subdomains } = req;
+
+  if (subdomains.length > 1) {
+    return res.status(404).send();
+  }
+
+  if (subdomains.length === 1) {
+    req.store = subdomains[0];
+    return store(req, res);
+  }
+
+  return platform(req, res);
 });
 
 const port = process.env.PORT || 5000;
